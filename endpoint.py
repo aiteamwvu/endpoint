0f718114be3b124aaa8e464f3cce1a61e842871f
mmaurier <mmaurier1@gmail.com>
2017-10-13 20:55:00 -0400

    add set_user

--- endpoint.py
+++ endpoint.py
@@ -62,7 +62,6 @@ def get_news(query):
 
 def set_user(email, keywords):
 	user = conn[config.col_users].find_one({"email": email})
-    
     keys = keywords.split(',')
     for key in keys:
         if not key in user["keywords"]:
